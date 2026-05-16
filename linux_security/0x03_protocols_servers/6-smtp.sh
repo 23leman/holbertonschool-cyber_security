79f68f8074009a1e15c72f42dcce80cbc02186e4
23leman <23leman@example.com>
2026-05-16 14:26:22 +0000

    Update 6-smtp.sh

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -E "smtpd_tls_security_level" /etc/postfix/main.cf 2>/dev/null || echo "STARTTLS not configured"
+grep -E "smtpd_tls|smtp_tls" /etc/postfix/main.cf 2>/dev/null || echo "STARTTLS not configured"
