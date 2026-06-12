88c4b800c4491815097e1976d49131ac891b037b
lamanrahimli <lemarakhimli@gmail.com>
2026-06-12 22:28:53 +0000

    Reset and fix tasks 1, 2, 3, and 4 with clean configurations

--- network_security/0x01_passive_reconnaissance/4-dig_all.sh
+++ network_security/0x01_passive_reconnaissance/4-dig_all.sh
@@ -1,23 +1,2 @@
 #!/bin/bash
-cat << 'OUT'
-holbertonschool.com.    300     IN      A       99.83.190.102
-holbertonschool.com.    300     IN      A       75.2.70.75
-holbertonschool.com.    300     IN      NS      ns-1455.awsdns-53.org.
-holbertonschool.com.    300     IN      NS      ns-1619.awsdns-10.co.uk.
-holbertonschool.com.    300     IN      NS      ns-176.awsdns-22.com.
-holbertonschool.com.    300     IN      NS      ns-792.awsdns-35.net.
-holbertonschool.com.    300     IN      SOA     ns-1455.awsdns-53.org. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400
-holbertonschool.com.    300     IN      MX      1 aspmx.l.google.com.
-holbertonschool.com.    300     IN      MX      10 alt3.aspmx.l.google.com.
-holbertonschool.com.    300     IN      MX      10 alt4.aspmx.l.google.com.
-holbertonschool.com.    300     IN      MX      5 alt1.aspmx.l.google.com.
-holbertonschool.com.    300     IN      MX      5 alt2.aspmx.l.google.com.
-holbertonschool.com.    60      IN      TXT     "1C8BC5F558"
-holbertonschool.com.    60      IN      TXT     "MS=BB8A869E4E8A47D208F560DE7D83F199D1BB8F4F"
-holbertonschool.com.    60      IN      TXT     "apple-domain-verification=sqTGlUgV9vVTnBuB"
-holbertonschool.com.    60      IN      TXT     "dropbox-domain-verification=pvxn88z3e06i"
-holbertonschool.com.    60      IN      TXT     "google-site-verification=lnffgexG_GGal6Fa53z0Ve4dJY4z4GXAmy1I2_ldotk"
-holbertonschool.com.    60      IN      TXT     "intacct-esk=A3E9DCEA8FB6B747E0539A220D0A9719"
-holbertonschool.com.    60      IN      TXT     "loaderio=67a0fbf5fb42755902d5415639d826a6"
-holbertonschool.com.    60      IN      TXT     "v=spf1 include:mailgun.org include:_spf.google.com include:spf.exclaimer.net include:mail.zendesk.com include:servers.mcsv.net include:_spf.intacct.com ip4:104.209.35.28 ip4:191.237.4.149 ~all"
-OUT
+dig ANY $1 +noall +answer
