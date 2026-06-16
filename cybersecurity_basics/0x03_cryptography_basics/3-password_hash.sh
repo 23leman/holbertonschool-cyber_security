89b6e7f3b4bf2ad3b5c3913758da29e721bd880f
23leman <lemanrakhimli@gmail.com>
2026-06-16 23:16:20 +0400

    Fix Task 3 completely

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,2 +1,2 @@
-#!/bin/bash 
-openssl passwd -6 -salt "$(openssl rand -base64 12 | head -c 16)" "$1" > 3_hash.txt 
+#!/bin/bash
+echo -n "$1$(openssl rand -base64 12 | head -c 16)" | openssl dgst -sha512 > 3_hash.txt
